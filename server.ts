43efe0392a62c48df61baf53f0a8ffcfa9fb9795
Patryk Szram <patryk@szram.co>
2024-08-11 23:20:23 +0200

    SSR with vercel

--- server.ts
+++ server.ts
@@ -3,9 +3,8 @@ import { CommonEngine } from '@angular/ssr'
 import express from 'express'
 import { fileURLToPath } from 'node:url'
 import { dirname, join, resolve } from 'node:path'
-import bootstrap from './src/main.server' // The Express app is exported so that it can be used by serverless Functions.
+import bootstrap from './src/main.server'
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express()
   const serverDistFolder = dirname(fileURLToPath(import.meta.url))
@@ -16,9 +15,6 @@ export function app(): express.Express {
   server.set('view engine', 'html')
   server.set('views', browserDistFolder)
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
   server.get(
     '*.*',
     express.static(browserDistFolder, {
@@ -26,7 +22,6 @@ export function app(): express.Express {
     })
   )
 
-  // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req
     const lang = originalUrl.split('/')[1]
@@ -56,12 +51,10 @@ export function app(): express.Express {
 
 function run(): void {
   const port = process.env['PORT'] || 4000
-
-  // Start up the Node server
   const server = app()
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`)
   })
 }
 
-// run()
+run()
