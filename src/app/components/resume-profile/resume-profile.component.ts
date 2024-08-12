7e0c01219d0895c4411026c3cf8726c7bd489097
Patryk Szram <patryk@szram.co>
2024-08-12 15:06:54 +0200

    Speed update

--- src/app/components/resume-profile/resume-profile.component.ts
+++ src/app/components/resume-profile/resume-profile.component.ts
@@ -48,7 +48,9 @@ export class ResumeProfileComponent extends AppDestroy implements OnInit, OnDest
       .pipe(takeUntil(this.destroy$))
       .subscribe((data) => {
         this.about = data
-        this.isReady = true
+        setTimeout(() => {
+          this.isReady = true
+        }, 2000)
       })
   }
 
